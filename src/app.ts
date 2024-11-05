43e077fe62f92e7ac10e7d66f2ebc162b311d3dc
Valerrriy Orlov <wrestlbird@gmail.com>
2024-11-05 18:45:27 +0300

    feat: safe shutdown added

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,14 @@ const app = Fastify({
   logger: true,
 });
 
+const listeners = ['SIGINT', 'SIGTERM'];
+listeners.forEach((signal) => {
+  process.on(signal, async () => {
+    await app.close();
+    process.exit(0);
+  });
+});
+
 async function main() {
   await app.listen({
     port: 8000,

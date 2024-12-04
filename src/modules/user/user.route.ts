d959080f981b8e0095f3a0045f662b170d3f0881
Valerrriy Orlov <wrestlbird@gmail.com>
2024-12-04 12:24:17 +0300

    feat: added root route prehandler and login route handler

--- src/modules/user/user.route.ts
+++ src/modules/user/user.route.ts
@@ -1,11 +1,14 @@
 import { FastifyInstance, FastifyRequest, FastifyReply } from "fastify";
 import { createUserResponseSchema, createUserSchema, loginResponseSchema, loginSchema } from "./user.schema";
-import { createUser } from "./user.controller";
+import { createUser, getUsers, login } from "./user.controller";
 
 export async function userRoutes(app: FastifyInstance) {
-  app.get('/', (req: FastifyRequest, reply: FastifyReply) => {
-    reply.send({ message: '/ route hit' });
-  });
+  app.get('/', 
+    {
+      preHandler: [app.authenticate],
+    },
+    getUsers,
+  );
 
   app.post('/register', {
     schema: {
@@ -26,7 +29,8 @@ export async function userRoutes(app: FastifyInstance) {
       },
     },
   },
-    () => {});
+  login,
+);
   app.delete('/logout', () => {});
   app.log.info('user routes registered');
 }
